52d063764e338f963b015ff97e0df78359e95356
Gia-Huynh <huynhthietgia123@gmail.com>
2023-02-23 13:54:00 +0700

    Reupload

---
+++ Gradient Descent/Multiple_Variable_Descent.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2
+import math
+
+def calcu (xx):
+    result=w1*xx+w2*math.sin(xx)
+    return (result)
+
+def loss (x1,x2):
+    return (abs(x1-x2))
+
+def true_ans (xx):
+    result=2*xx+2*math.sin(xx)
+    return (result)
+    
+#2x+2sin(x)
+true=2*4+2*math.sin(4)
+#4x+3sin(x)
+w1=4
+w2=3
+learning_rate=0.025
+i=0
+while True:
+    x=(i%10)+1
+    Y= true_ans (x)
+    print (w1,' ',w2)
+    w1=w1+learning_rate*2*x*(Y-calcu(x))
+    w2=w2+learning_rate*2*math.sin(x)*(Y-calcu(x))
+    i=i+1
+    if i==100:
+        break
+print (w1,' ',w2)
+print (calcu(4),' ',Y)
---
+++ README.md
@@ -0,0 +1,3 @@
+# ML_coban
+Vào thư mục Colab để chạy file live interaction Machine Learning.
+Thư mục Gradient Descent là tạo ảnh visualize đạo hàm.
